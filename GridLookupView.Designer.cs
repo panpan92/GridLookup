c4952c60fa091a2e47bd9c48bdb99d2531dd9ab7
panpan92 <599306174@qq.com>
2018-07-26 17:50:41 +0800

    Add files via upload

---
+++ GridLookupView.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Ecis.Infrastructure.UI.Controls
+{
+    partial class GridLookupView
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fProperties = new DevExpress.XtraEditors.Repository.RepositoryItemGridLookUpEdit();
+            this.fPropertiesView = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridView2 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridView3 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridView4 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.GridLookupViewView = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.fProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fPropertiesView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GridLookupViewView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fProperties
+            // 
+            this.fProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.fProperties.Name = "fProperties";
+            this.fProperties.View = this.GridLookupViewView;
+            // 
+            // fPropertiesView
+            // 
+            this.fPropertiesView.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.fPropertiesView.Name = "fPropertiesView";
+            this.fPropertiesView.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.fPropertiesView.OptionsView.ShowGroupPanel = false;
+            // 
+            // gridView1
+            // 
+            this.gridView1.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            // 
+            // gridView2
+            // 
+            this.gridView2.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.gridView2.Name = "gridView2";
+            this.gridView2.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.gridView2.OptionsView.ShowGroupPanel = false;
+            // 
+            // gridView3
+            // 
+            this.gridView3.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.gridView3.Name = "gridView3";
+            this.gridView3.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.gridView3.OptionsView.ShowGroupPanel = false;
+            // 
+            // gridView4
+            // 
+            this.gridView4.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.gridView4.Name = "gridView4";
+            this.gridView4.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.gridView4.OptionsView.ShowGroupPanel = false;
+            // 
+            // GridLookupViewView
+            // 
+            this.GridLookupViewView.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.GridLookupViewView.Name = "GridLookupViewView";
+            this.GridLookupViewView.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.GridLookupViewView.OptionsView.ShowGroupPanel = false;
+            ((System.ComponentModel.ISupportInitialize)(this.fProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fPropertiesView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GridLookupViewView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.Repository.RepositoryItemGridLookUpEdit fProperties;
+        private DevExpress.XtraGrid.Views.Grid.GridView fPropertiesView;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView2;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView3;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView4;
+        private DevExpress.XtraGrid.Views.Grid.GridView GridLookupViewView;
+    }
+}
---
+++ GridLookupView.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Controls;
+using DevExpress.XtraGrid.Columns;
+
+namespace Ecis.Infrastructure.UI.Controls
+{
+    public partial class GridLookupView : GridLookUpEdit
+    {
+        public delegate void CreateDate(string value);
+        public event CreateDate newDate;
+        public GridLookupView()
+        {
+            InitializeComponent();
+            InitDefaultValue();
+        }
+
+        private void InitDefaultValue()
+        {
+            this.Properties.TextEditStyle = TextEditStyles.Standard;
+            this.Properties.ImmediatePopup = true;
+            this.ProcessNewValue += gridLookUpEdit_ProcessNewValue;
+            //SetGridLookUpEditMoreColumnFilter(this.Properties);
+        }
+        private void gridLookUpEdit_ProcessNewValue(object sender, ProcessNewValueEventArgs e)
+        {
+            var sender1 = ((GridLookUpEdit)sender);
+            if (sender1 != null)
+            {
+                var Edit = sender1.Properties;
+                if (e.DisplayValue == null || Edit.NullText.Equals(e.DisplayValue) || string.Empty.Equals(e.DisplayValue))
+                    return;
+                if (newDate != null)
+                {
+                    newDate(e.DisplayValue.ToString());
+                }
+                e.Handled = true;
+            }
+
+        }
+         //<summary>
+         //设置GridLookUpEdit多列过滤
+         //</summary>
+         //<param name="repGLUEdit">GridLookUpEdit的知识库，eg:gridlookUpEdit.Properties</param>
+        public void SetGridLookUpEditMoreColumnFilter(DevExpress.XtraEditors.Repository.RepositoryItemGridLookUpEdit repGLUEdit)
+        {
+            repGLUEdit.EditValueChanging += (sender, e) =>
+            {
+                this.BeginInvoke(new System.Windows.Forms.MethodInvoker(() =>
+                {
+                    GridLookUpEdit edit = sender as GridLookUpEdit;
+                    DevExpress.XtraGrid.Views.Grid.GridView view = edit.Properties.View as DevExpress.XtraGrid.Views.Grid.GridView;
+                    //获取GriView私有变量
+                    System.Reflection.FieldInfo extraFilter = view.GetType().GetField("extraFilter", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+                    List<DevExpress.Data.Filtering.CriteriaOperator> columnsOperators = new List<DevExpress.Data.Filtering.CriteriaOperator>();
+                    foreach (GridColumn col in view.VisibleColumns)
+                    {
+                        //这里判断哪一种条件的列头是否可以进行搜索，你也可以自己修改自己需要的情况！
+                        if (col.Visible && col.ColumnType == typeof(string))
+                            columnsOperators.Add(new DevExpress.Data.Filtering.FunctionOperator(DevExpress.Data.Filtering.FunctionOperatorType.Contains,
+                                new DevExpress.Data.Filtering.OperandProperty(col.FieldName),
+                                new DevExpress.Data.Filtering.OperandValue(edit.Text)));
+                    }
+
+                    string filterCondition = new DevExpress.Data.Filtering.GroupOperator(DevExpress.Data.Filtering.GroupOperatorType.Or, columnsOperators).ToString();
+
+                    extraFilter.SetValue(view, filterCondition);
+                    //获取GriView中处理列过滤的私有方法
+                    System.Reflection.MethodInfo ApplyColumnsFilterEx = view.GetType().GetMethod("ApplyColumnsFilterEx", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                    ApplyColumnsFilterEx.Invoke(view, null);
+                }));
+            };
+        }
+
+       
+
+       
+    }
+}
